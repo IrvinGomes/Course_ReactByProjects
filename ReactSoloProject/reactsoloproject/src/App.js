28c91af3bc2ff6c43dc0c46e7c7e905be643a03f
IrvinGomes <irvin.r.gomes@outlook.com>
2022-06-07 23:42:32 -0300

    adding images and content to my card

--- ReactSoloProject/reactsoloproject/src/App.js
+++ ReactSoloProject/reactsoloproject/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Card from './components/Card';
 
 export default function App() {
     return (
-        <h1>App is working</h1>
+        <div className='app'>
+            <Card />
+        </div>
     )
 }
---
+++ ReactSoloProject/reactsoloproject/src/components/CardFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/CardFooter.css'
+
+export default function CardFooter() {
+    return (
+        <div className="footer">
+            <div></div>
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25347.png" />
+            <img src="https://cdn-icons-png.flaticon.com/512/1051/1051309.png" />
+            <img src="https://cdn-icons-png.flaticon.com/512/1400/1400829.png" />
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25657.png" />
+            <div></div>
+        </div>
+    )
+}
