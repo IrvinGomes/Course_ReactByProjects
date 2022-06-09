e0e869bb20cbf4760dd80c09cffafa8c96db7474
IrvinGomes <irvin.r.gomes@outlook.com>
2022-06-09 11:25:45 -0300

    header done

--- ReactSoloProject2/reactsoloproject2/src/components/Header.js
+++ ReactSoloProject2/reactsoloproject2/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+import '../styles/Header.css'
+
 export default function Header() {
     return (
-        <h1>Header Works!</h1>
+        <div className="header">
+            <img className="header--img" src="https://cdn-icons-png.flaticon.com/512/44/44536.png" alt="" />
+            <h3 className="header--text">My Travel Journal</h3>
+        </div>
     )
 }
