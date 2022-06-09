3e3738ce59b197f6d84de09c8b14a38941c2e596
IrvinGomes <irvin.r.gomes@outlook.com>
2022-06-09 15:14:53 -0300

    added meme form

---
+++ ReactDynamicPage/memegenerator/src/components/MemeForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/MemeForm.css';
+
+export default function MemeForm() {
+    return (
+        <form className="memeForm">
+            <input type="text" />
+            <input type="text" />
+            <input type="submit" />
+        </form>
+    )
+}
