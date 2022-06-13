88c600c8700f65a92e89775117b48e07c28ec885
IrvinGomes <irvin.r.gomes@outlook.com>
2022-06-13 20:16:34 -0300

    adding useState to change the text

--- ReactDynamicPage/memegenerator/src/components/MemeForm.js
+++ ReactDynamicPage/memegenerator/src/components/MemeForm.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/MemeForm.css';
+import Data from '../data/memesData.js'
 
 export default function MemeForm() {
+    const memes = Data.data.memes;
+    let [meme, setMeme] = useState();
+    let [top_text, setTopText] = useState();
+    let [bottom_text, setBottomText] = useState();
+
+    function getMeme() {
+        setMeme(memes[Math.floor(Math.random() * memes.length)])
+    }
+
+    function getTopText(event) {
+        setTopText(event.target.value)
+    }
+    function getBottomText(event) {
+        setBottomText(event.target.value)
+    }
+
     return (
         <div>
-            <form className="memeForm">
-                <input className="memeForm--input" type="text" placeholder="Top Text" />
-                <input className="memeForm--input" type="text" placeholder="Bottom text" />
-                <button className="memeForm--submit">Get a new meme image 🖼️</button>
-            </form>
+            <div className="memeForm" action="#">
+                <input
+                    className="memeForm--input"
+                    type="text"
+                    placeholder="Top Text"
+                    onChange={getTopText} />
+                <input
+                    className="memeForm--input"
+                    type="text"
+                    placeholder="Bottom text"
+                    onChange={getBottomText} />
+                <button
+                    className="memeForm--submit"
+                    onClick={getMeme}>
+                    Get a new meme image 🖼️
+                </button>
+            </div>
+            <div className="memeImage">
+                <p className="memeImage--top_text">{top_text}</p>
+                {meme && <img src={meme.url} alt="" />}
+                <p className="memeImage--bottom_text">{bottom_text}</p>
+            </div>
         </div>
     )
 }
